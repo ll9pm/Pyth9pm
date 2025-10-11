c614bfb3b7f6f8232849482a1db436ac620f1c6a
ll9pm <aboodll9pm@gmail.com>
2025-10-11 23:37:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,18 @@ def send_request(api):
 for i in range(110):
 	# Create a list of threads
 	threads = []
-	apis = ["ce584754-f243-4ee4-9bc5-e990ed2662ae",
-	"3a021d5e-5858-4395-830f-1ede1821cffb",
-	"071b79d0-0d5e-4b4c-898e-3c02200121ac",
-	"dc7cc5ec-44fe-4734-a772-dff36add85d3",
-	"2aea1ea3-4c50-4cf1-a453-3c28f289fcaf"
-	]  # Number of parallel requests
+	apis = ["6199463b-befe-4207-a3e6-ca747574c2ee",
+	"dd39d2b6-b0f0-45f4-afa1-0f7a1c98ecf1",
+	"0717ae64-caa0-4e26-94cf-9801e0c92b7a",
+	"07db26ca-9b58-4e12-af52-59752b2c5fa3",
+	"a0d911ca-8cc7-40ed-813b-5822d65913cb",
+	"31e54535-d3dd-43a0-9bcc-32e228c4f978",
+	"c1a8c6be-bc01-405d-a356-4aeb9c6d1cbb",
+	"e22809cb-1050-4d8a-9dca-7b8d482c752d",
+	"887170a8-e390-40a0-a10d-38a69f4f2340",
+	"e9db5718-d58d-4bc9-9241-d7e08be1b42c",
+	"a2664670-f890-4379-a189-5f8f96080e95"
+	] 
 	
 	for api in apis:
 	    thread = threading.Thread(target=send_request, args=(api,))

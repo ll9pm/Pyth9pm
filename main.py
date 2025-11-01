ad0c4ce3215491bf97dc2a461833e9c610f573b5
ll9pm <aboodll9pm@gmail.com>
2025-11-01 15:07:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def run(playwright: Playwright):
     playwright = await async_playwright().start()
     browser = await playwright.chromium.connect_over_cdp("wss://browser.scrapeless.com/browser?token=sk_m1UhOAh9CRNRRlvoFSpPrfQmJS4BTfZyq4vk7BM5CD5CJEXXD4HSD3hcTTggsQcn&session_name=sdk_test&session_ttl=180&session_recording=True&proxy_country=US")
     page = await browser.contexts[0].new_page()
-    await page.goto("https://viikqoye.com/dc/?blockID=402349")
+    await page.goto("https://short.do/vvq8yH")
     await asyncio.sleep(randint(10,25))
 
     #await page.screenshot(path="screenshot.png", full_page=True)# end session

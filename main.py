2d58503545721c24c36452ec361fa169ff94cfa4
ll9pm <aboodll9pm@gmail.com>
2025-11-01 08:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 
-
-from time import sleep
+import nest_asyncio
+nest_asyncio.apply()
+import asyncio
 from random import randint
-from playwright.sync_api import sync_playwright, Playwright
+# start browser, not to use 'with' context
+from playwright.async_api import async_playwright
+for i in range(100):
+ try:
+  playwright = await async_playwright().start()
+  browser = await playwright.chromium.connect_over_cdp("wss://browser.scrapeless.com/browser?token=sk_jRXfVrBSwsQ3SlpKTbXt1r6NWy2jvjj3a51BAwbrJZ09l8vp1Rzu1ohZEDNZIZNd&session_name=sdk_test&session_ttl=180&session_recording=True&proxy_country=US")
+  page = await browser.contexts[0].new_page()
+  
+  await page.goto("https://viikqoye.com/dc/?blockID=399132",timeout=40)
+  await asyncio.sleep(randint(10,25))
 
-def run(playwright: Playwright):
-   playwright =  sync_playwright().start()
-   browser =  playwright.chromium.connect_over_cdp("wss://browser.scrapeless.com/browser?token=sk_jRXfVrBSwsQ3SlpKTbXt1r6NWy2jvjj3a51BAwbrJZ09l8vp1Rzu1ohZEDNZIZNd&session_name=sdk_test&session_ttl=180&session_recording=True&proxy_country=US")
-   page =  browser.contexts[0].new_page()
-   page.goto("https://viikqoye.com/dc/?blockID=399132")
-   sleep(randint(10,25))
-
-   #await page.screenshot(path="screenshot.png", full_page=True)# end session
-   browser.close()
-   playwright.stop()
-with sync_playwright() as playwright:
-    run(playwright)
+  #await page.screenshot(path="screenshot.png", full_page=True)# end session
+  await browser.close()
+  await playwright.stop()
+  print(i)
+ except Exception as e:
+   print ("error",e)
